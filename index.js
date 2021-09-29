82308eb027d6993b1ba28ce959c737f86320744e
mrssmith <marisolbsmith@yahoo.com>
2021-09-29 09:01:12 -0500

    add packages

---
+++ index.js
@@ -0,0 +1,6 @@
+// Required Packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
